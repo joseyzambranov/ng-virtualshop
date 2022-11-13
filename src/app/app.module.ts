c1ec998928b766c00d6fad6438bc3224a6093fe8
joseyzambranov <joseyzambranovpe@gmail.com>
2022-11-12 21:58:27 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { FeaturedComponent } from './components/featured/featured.component';
+import { OfferComponent } from './components/offer/offer.component';
+import { NewsletterComponent } from './components/newsletter/newsletter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    FeaturedComponent,
+    OfferComponent,
+    NewsletterComponent
   ],
   imports: [
     BrowserModule,
